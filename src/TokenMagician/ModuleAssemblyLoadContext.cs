2fcdd6e32f2f94b17abd8482972c0aaed8a0b0d6
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2024-12-27 12:38:53 +0100

    AssembyContexts are no fun

---
+++ src/TokenMagician/ModuleAssemblyLoadContext.cs
@@ -0,0 +1,33 @@
+namespace TokenMagician;
+using System.Reflection;
+using System.Runtime.Loader;
+
+internal class ModuleAssemblyLoadContext : AssemblyLoadContext
+    {
+        private readonly string _dependencyDirPath;
+
+        public ModuleAssemblyLoadContext(string dependencyDirPath)
+        {
+            _dependencyDirPath = dependencyDirPath;
+        }
+
+        protected override Assembly Load(AssemblyName assemblyName)
+        {
+            // We do the simple logic here of looking for an assembly of the given name
+            // in the configured dependency directory.
+            string assemblyPath = Path.Combine(
+                _dependencyDirPath,
+                $"{assemblyName.Name}.dll");
+
+            if (File.Exists(assemblyPath))
+            {
+                Console.WriteLine($"Loading assembly {assemblyName.Name} from {assemblyPath}");
+                // The ALC must use inherited methods to load assemblies.
+                // Assembly.Load*() won't work here.
+                return LoadFromAssemblyPath(assemblyPath);
+            }
+
+            // For other assemblies, return null to allow other resolutions to continue.
+            return null;
+        }
+    }
