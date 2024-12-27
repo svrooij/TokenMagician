11a0b1550f56cfaf87bed70ed7635d8a2357b8cf
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2024-12-27 14:33:00 +0100

    Let's try that again (#2)
    * Improve tests

--- tests/TokenMagician/Get-TmMsiToken.Tests.ps1
+++ tests/TokenMagician/Get-TmMsiToken.Tests.ps1
@@ -3,4 +3,12 @@ Describe 'Get-TmMsiToken' {
       $cmdlet = Get-Command -Name 'Get-TmMsiToken'
       $cmdlet.CommandType | Should -Be 'Cmdlet'
   }
+
+  It 'Should throw an error if no arguments are provided' {
+    { Get-TmMsiToken } | Should -Throw
+  }
+
+  It 'Should throw an error if the Client ID is not a GUID' {
+    { Get-TmMsiToken -TenantId xxx -ClientId 'not-a-guid' -Scope https://graph.microsoft.com./default } | Should -Throw
+  }
 }
